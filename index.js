f4021b9c4c8039db84bdc4891fe2895ee7781a52
Stuart Dear <stuart.dear@intermarkgroup.com>
2018-02-22 11:47:19 -0600

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+
+const express = require('express');
+
+const app = express(); //we call express() like a function as we might have multiple express servers in our application. 
+
+//a route handler
+app.get('/', (req, res) => {
+    //response as JSON
+    res.send({ hi: 'there' });
+});
+
+//Dynamical assign port Heroku. If runtime env config var not avail, use 5000 for local dev.  
+const PORT = process.env.PORT || 5000;
+//what port to listen at
+app.listen(PORT);
+//go to term >> node index.js
+//then nav to localhost:5000
