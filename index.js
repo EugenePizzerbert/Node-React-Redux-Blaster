e815e6cc9763b1257d51f823f1b80cb3331efe94
Stuart Dear <stuart.dear@intermarkgroup.com>
2018-03-05 13:09:44 -0600

    progress update

--- index.js
+++ index.js
@@ -1,13 +1,35 @@
 
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
 
 const app = express(); //we call express() like a function as we might have multiple express servers in our application. 
 
-//a test route handler
-app.get('/', (req, res) => {
-    //response as JSON
-    res.send({ bye: 'buddy' });
-});
+passport.use(
+        new GoogleStrategy(
+        {
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: '/auth/google/callback'
+        }, 
+        accessToken => {
+            console.log(accessToken);
+        }
+    )
+);
+
+//Auth request to google
+app.get('/auth/google', passport.authenticate('google', {
+    scope: ['profile', 'email']
+})
+);
+
+//Route for callback, Google response (with Code ID as guid )
+app.get('/auth/google/callback', passport.authenticate('google'));
+
+
+
 
 //Dynamical assign port Heroku. If runtime env config var not avail, use 5000 for local dev.  
 const PORT = process.env.PORT || 5000;
