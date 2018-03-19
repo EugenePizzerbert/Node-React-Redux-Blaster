0f4696371fabc67286c68d33b6a38a89b343fab1
Stuart Dear <stuart.dear@intermarkgroup.com>
2018-03-19 10:09:52 -0500

    milestone: Dev duplex, added react server

--- services/passport.js
+++ services/passport.js
@@ -24,20 +24,17 @@ passport.use(
         callbackURL: '/auth/google/callback',
         proxy: true
     }, 
-    (accessToken, refreshToken, profile, done) => {
-        //Check if user already exists
-        User.findOne({ googleId: profile.id })
-            .then((existingUser) => {
-                if (existingUser) {
-                //exist TRUE, we already have a record with the given profile.
-                done(null, existingUser);
-                } else {
-                  //Create and save a new user
-                  new User({ googleId: profile.id})
-                    .save()
-                    .then(user => done(null, user));
-                }
-            }) 
+    async (accessToken, refreshToken, profile, done) => {
+        const existingUser = await User.findOne({ googleId: profile.id })
+        
+        if (existingUser) {
+        return done(null, existingUser);
+        } 
+
+        const user = await new User({ googleId: profile.id}).save()
+        done(null, user);
+        
+          
     }
 )
 );
